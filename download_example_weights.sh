e19bd81773278598fcca1ac601ca7f3a5c48444d
RuchithaManne <68728304+RuchithaManne@users.noreply.github.com>
2022-06-18 21:34:00 +0530

    Update download_example_weights.sh
    Storing logs to drive in colab

--- download_example_weights.sh
+++ download_example_weights.sh
@@ -1,5 +1,5 @@
-mkdir -p logs
-cd logs
+mkdir -p /content/drive/MyDrive/An_output_Nerf_1806/logs
+cd /content/drive/MyDrive/An_output_Nerf_1806/logs
 wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/fern_example_weights.zip
 unzip fern_example_weights.zip
 wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/lego_example_weights.zip
